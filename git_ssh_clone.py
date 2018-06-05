f92c22ee17c7b92480f004a221fc464f178ba258
Nick Spain <nicholas.spain96@gmail.com>
2018-06-05 14:16:53 +1000

    Add script for converting HTTP git URI to SSH git URI

---
+++ git_ssh_clone.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""Convert an HTTP url to the SSH equivalent and clone the repo.
+
+https://github.com/emacs-lsp/lsp-python.git
+git@github.com:emacs-lsp/lsp-python.git
+
+"""
+
+import sys
+import subprocess
+
+
+def is_ssh(uri):
+    """Check if URI is a git SSH URI."""
+    return uri.split('@')[-1] == 'git'
+
+
+def to_ssh(uri):
+    """convert a URI to an SSH URI."""
+    ssh_uri = uri.split('://')[-1].split('/')
+    ssh_uri = f'git@{ssh_uri[0]}:{"/".join(ssh_uri[1:])}'
+    return ssh_uri
+
+
+def main(args):
+    """Entry point."""
+    if len(args) < 2:
+        return 1
+    uri = args[-1]
+    if not is_ssh(uri):
+        uri = to_ssh(uri)
+    cmd = ['git', 'clone']
+    cmd += args[1:-1]
+    cmd.append(uri)
+    subproc = subprocess.run(cmd)
+    return subproc.returncode
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
